667258e98248d87f965e9a54da84d53513eccf05
achutman <56025467+achutman@users.noreply.github.com>
2021-04-08 09:48:39 +0100

    Update gpUniMultiOutRefugeeAsylumScoreScript.py
    Replaced with additional comments aligned to the methodology document.

--- scripts/gpUniMultiOutRefugeeAsylumScoreScript.py
+++ scripts/gpUniMultiOutRefugeeAsylumScoreScript.py
@@ -6,7 +6,7 @@ Created on Thu 18 March 2021
 Contact: William Low, Project Lead, Migration and Displacement Initiative, Save the Children International
 
 The script compares the outputs of gpUniOutRefugeeAsylumScript.py and gpMultiOutRefugeeAsylumScript.py,
-i.e. the outputs of univariate vs. multivariate GP modelling.
+i.e. the outputs of uni-output vs. multi-output GP modelling.
 """
 import os
 import numpy as np
@@ -16,7 +16,7 @@ import xlrd
 
 
 # Select countries based on INFORM index
-pathInform = r'...'
+pathInform = r'C:\Users\a.manandhar\OneDrive - Save the Children International\Documents\data\INFORM'
 dfInform = pd.read_csv(os.path.join(pathInform,'raw','INFORM2020_TREND_2010_2019_v040_ALL_2 INFORMRiskIndex.csv'))
 countries = dfInform.loc[dfInform['2020']>=5,'Iso3'].values
 cntySkip = ['PRK','PNG','PSE','SOM']
@@ -24,10 +24,10 @@ for cnty in countries:
     if cnty in cntySkip:        
         countries = np.delete(countries,np.where(countries==cnty))
 
-# Outputs saved from gpUniOutRefugeeAsylumScript.py
-pathSaveUni = r'...'
-# Outputs saved from gpMultiOutRefugeeAsylumScript.py
-pathSaveMulti = r'...'
+# Path to uni-Out model outputs saved from gpUniOutRefugeeAsylumScript.py
+pathSaveUni = r'C:\Users\a.manandhar\OneDrive - Save the Children International\Documents\docs\PD phase two model\gaussian process\dataPlots\UNHCR RAstk\timeSepCoregRbfLsFixed3MaxTest2017Nahead1'
+# Path to multi-out outputs saved from gpMultiOutRefugeeAsylumScript.py
+pathSaveMulti = r'C:\Users\a.manandhar\OneDrive - Save the Children International\Documents\docs\PD phase two model\gaussian process\dataPlots\UNHCR RAstkMstkRretRAflowRemUfat\timeSepCoregRbfLsFixed3MaxTest2017Nahead1'
 
 YEAR_TEST = 2017
 Y_RAstk_Uni = pd.DataFrame()
@@ -36,19 +36,22 @@ Y_RAstk_Uni = []
 Y_RAstk_Multi = []
 for cnty in countries:          
     print(cnty)            
-    # Ref
+    # Reference = Uni-Output model
     Yout = pd.read_excel(os.path.join(pathSaveUni,cnty,'data_outputs.xlsx'),sheet_name='outputs')    
     # idxTest = ((Yout['Year']>=2017) & (Yout['Year']<=2018))
     idxTest = (Yout['Year']>=2017)
     Y_RAstk_Uni = Y_RAstk_Uni.append(Yout.loc[idxTest,['Year', 'Y', 'Ymu', 'Yvar', 'YmuMinus','YmuPlus']])            
     Y_RAstk_Uni.extend([cnty for i in range(sum(idxTest))])
-    # Compare to
+    # Compare to Multi-Output model
     Yout = pd.read_excel(os.path.join(pathSaveMulti,cnty,'data_outputs.xlsx'),sheet_name='outputs')
     # idxTest = ((Yout['Year']>=2017) & (Yout['Year']<=2018) & (Yout['VarName']=='RAstk'))
     idxTest = ((Yout['Year']>=2017) & (Yout['VarName']=='RAstk'))
     Y_RAstk_Multi = Y_RAstk_Multi.append(Yout.loc[idxTest,['Year', 'Y', 'Ymu', 'Yvar', 'YmuMinus','YmuPlus']])            
     Y_RAstk_Multi.extend([cnty for i in range(sum(idxTest))])
 
+# AE = Absolute Error
+# APE = Absolute Percentage Error
+# CI = 95% Confidence Interval
 Y_RAstk_Uni['AE'] = np.round_(np.abs(Y_RAstk_Uni['Ymu']-Y_RAstk_Uni['Y']))
 Y_RAstk_Uni['APE'] = np.round_(100*np.abs(Y_RAstk_Uni['Ymu']-Y_RAstk_Uni['Y'])/Y_RAstk_Uni['Y'])
 Y_RAstk_Uni['CI'] = np.round_(Y_RAstk_Uni['YmuPlus']-Y_RAstk_Uni['YmuMinus'])
@@ -68,9 +71,11 @@ Y_RAstk_Multi = Y_RAstk_Multi.reset_index().drop('index',axis=1)
 # Y_RAstk_Multi = Y_RAstk_Multi.set_index('Country')
 # Y_RAstk_Multi.to_csv(os.path.join(pathSaveMulti,'score.csv'))
 
+# Aggregate scores
 print(Y_RAstk_Uni.loc[:,['Year','AE','APE','CI']].groupby('Year').median())
 print(Y_RAstk_Multi.loc[:,['Year','AE','APE','CI']].groupby('Year').median())
 
+# Plot score comparison
 SAVE_OPTION = 0
 for year in [2017,2018]:
     fig, ax = plt.subplots()
